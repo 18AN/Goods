3e1de8d41c815f7f360d4680b63c3d0f5a8c9abc
Ivan Volkovets <batmad857@gmail.com>
2022-08-02 03:01:01 +0300

    add stylization and adaptive layout

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,21 @@
 <template>
     <form @submit.prevent>
-        <my-input v-model="item.title" type="text" placeholder="Наименование товара"/>
-        <p v-if="!titleIsValid">Поле является обязательным и должно содержать только буквы</p>
+        <div class="input-wrapper">
+            <div class="input__title">Наименование товара</div>
+            <my-input v-model="item.title" type="text" placeholder="Введите наименование" :class="{error: !titleIsValid}"/>
+            <p v-if="!titleIsValid" class="hint">Поле является обязательным и должно содержать только буквы</p> 
+        </div>
         <textarea v-model="item.description"></textarea>
-        <my-input v-model="item.link" type="text" placeholder="Ссылка на изображение"/>
-        <p v-if="!linkIsValid">Поле является обязательным(более 1 символа.домен)без!$^*;'"` </p>
-        <my-input v-model="item.price" type="text" placeholder="Цена товара"/>
-        <p v-if="!priceIsValid">Поле является обязательным и должно содержать только цифры </p>
+        <div class="input-wrapper">
+            <div class="input__title">Ссылка на изображение товара</div>
+            <my-input v-model="item.link" type="text" placeholder="Введите ссылку" :class="{error: !linkIsValid}"/>
+            <p v-if="!linkIsValid" class="hint">Поле является обязательным(более 1 символа.домен)без!$^*;'"` </p>
+        </div>
+        <div class="input-wrapper">
+            <div class="input__title">Цена товара</div>
+            <my-input v-model="item.price" type="text" placeholder="Введите цену" :class="{error: !priceIsValid}"/>
+            <p v-if="!priceIsValid" class="hint">Поле является обязательным и должно содержать только цифры </p>
+        </div>
         <button class="form__button" :disabled="!formIsValid" @click="createItem">Добавить товар</button>
     </form>
 </template>
@@ -58,18 +67,76 @@ export default {
 <style lang="scss" scoped>
 form{
     max-width: 20.75em;
-    padding: 1.5em 1.5em;
+    height: fit-content;
+    padding: 1.5em;
+    border-radius: 0.25em;
 }
 
 textarea{
     resize: none;
+    outline: none;
+    line-height: 1.25em;
     width: 100%;
-    min-height: 6.75em;
+    min-height: 9em;
+    margin-bottom: 1.33em;
     display: block;
-    overflow: hidden;
+    overflow: auto;
+    padding: 0.83em 1.33em 0;
+    border: none;
+    border-radius: 0.33em;
+    box-shadow: 0em 0.16em 0.41em rgb(0 0 0 / 10%);
 }
 
 .form__button{
-    width: 100%;
+    text-align: center;
+    font-family: 'Inter', sans-serif;
+    font-size: 0.75em;
+    min-height: 3em;
+    line-height: 1.25em;
+    margin-top: 0.33em;
+    border: none;
+    border-radius: 0.83em;
+    background-color: #7BAE73;
+    color: #FFFFFF;
+    box-shadow: 0px 0.16em 0.33em rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    transition: 500ms;
+}
+
+.form__button:disabled{
+    background-color: #EEEEEE;;
+    color: #B4B4B4;
+    box-shadow: none;
+}
+
+.error{
+    border: 0.08em solid #FF8484;
+    color:#FF8484;
+}
+
+.hint{
+    font-size: 0.65em;
+    letter-spacing: -0.01em;
+    color: #FF8484;
+    margin-top: 0.38em;
+}
+
+.input-wrapper{
+    margin-bottom: 1em;
+}
+.input__title{
+    display: flex;
+    font-size: 0.7em;
+    margin-bottom: 0.36em;
+    line-height: 1.3em;
+}
+
+.input__title::after{
+    content: '';
+    flex: 0 0 0.4em;
+    height: 0.4em;
+    background-color: #FF8484;
+    border-radius: 50%;
+    transform: translateX(0.2em);
 }
 </style>
